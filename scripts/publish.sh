e7c171d731da6154fa7258bf015568f9ba51aaf0
Iván Corrales Solera <developer@wesovilabs.com>
2017-12-14 06:16:38 +0100

    feat: adding publishs cript

---
+++ scripts/publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DOCKER_REGISTRY="wildnature"
+MODULE=$1
+VERSION=$(cat .version)
+
+@docker login -u "$(DOCKER_USER)" -p "$(DOCKER_PASS)"
+docker push $DOCKER_REGISTRY/macaque-$MODULE:$VERSION
