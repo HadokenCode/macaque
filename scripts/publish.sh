c2cd8b86d16a407fdcbf03afab8301fbc8adb36d
Iván Corrales Solera <developer@wesovilabs.com>
2017-12-14 07:37:43 +0100

    feat: golint and go vet in the same step

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,5 @@ DOCKER_REGISTRY="wildnature"
 MODULE=$1
 VERSION=$(cat .version)
 
-@docker login -u "$(DOCKER_USER)" -p "$(DOCKER_PASS)"
+docker login -u "$(DOCKER_USER)" -p "$(DOCKER_PASS)"
 docker push $DOCKER_REGISTRY/macaque-$MODULE:$VERSION
